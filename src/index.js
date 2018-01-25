ec4d0342d715b7b3b0a7b1ad55fdab2af155065b
Sandor Bereczki <bsandor453@gmail.com>
2018-01-25 17:38:21 +0100

    initial page layout created

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
 
 import configureStore from './configureStore';
 import './index.css';
