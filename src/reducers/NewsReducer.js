bf476c8b3d4dd7bc72d64a158ec0f1bc91932efa
Tamás Szabó <tamas@szaboo.com>
2018-02-05 19:45:28 +0100

    possible fix for duplicated news

--- src/reducers/NewsReducer.js
+++ src/reducers/NewsReducer.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = [];
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case GET_NEWS:
-      return [ ...state, ...action.payload ];
+      return action.payload;
     default:
       return state;
   }
