a1e7bce4915ac236df0524dadde29ec72d2ebe39
Chif Gergő <chifgeri97@gmail.com>
2018-12-18 21:53:03 +0100

    Change delete in AddNewsReducer

--- src/reducers/NewsReducer.js
+++ src/reducers/NewsReducer.js
@@ -9,10 +9,8 @@ export default (state = INITIAL_STATE, action) => {
     case ADD_NEWS:
       return [action.payload, ...state];
     case DELETE_NEWS:
-      const index = state.indexOf(action.payload);
-      const array = [...state];
-      array.splice(index, 1);
-      return [...array];
+      state.splice(state.indexOf(action.payload), 1);
+      return [...state];
     default:
       return state;
   }
