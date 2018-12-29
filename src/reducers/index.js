4a587c3e158fe4cb949dab5bd92c71b9b5e6c5e5
Chif Gergő <chifgeri97@gmail.com>
2018-12-29 19:53:48 +0100

    Add event reducer to add events to state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import UserReducer from './UserReducer';
 import NewsReducer from './NewsReducer';
 import AddNewsReducer from './AddNewsReducer';
 import EditNewsReducer from './EditNewsReducer';
+import EventReducer from './EventReducer';
 
 const rootReducer = combineReducers({
   user: UserReducer,
   news: NewsReducer,
   newNews: AddNewsReducer,
   selectedNews: EditNewsReducer,
+  events: EventReducer,
 });
 
 export default rootReducer;
