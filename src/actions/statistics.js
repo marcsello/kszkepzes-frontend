97027dc362e5756fa1e94aef1dc1c43664d6f4b1
Chif Gergő <chifgeri97@gmail.com>
2019-01-05 21:19:00 +0100

    Add actions to handle visitors of an event

--- src/actions/statistics.js
+++ src/actions/statistics.js
@@ -1,5 +1,5 @@
 import { axios } from './auth';
-import { GET_EVENTS, GET_EVENT_BY_ID } from './types';
+import { GET_EVENTS, GET_EVENT_BY_ID, GET_TRAINEES, VISITOR_CHANGE, GET_NOTES_BY_EVENT } from './types';
 
 export const getEvents = () => (
   async (dispatch) => {
@@ -22,6 +22,50 @@ export const getEventById = id => (
       dispatch({
         type: GET_EVENT_BY_ID,
         payload: response.data,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+);
+
+export const getTrainees = () => (
+  async (dispatch) => {
+    try {
+      const response = await axios.get('/api/v1/profiles/');
+      dispatch({
+        type: GET_TRAINEES,
+        payload: response.data,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+);
+
+export const visitorChange = visitors => (
+  dispatch => (dispatch({ type: VISITOR_CHANGE, payload: visitors }))
+);
+
+export const getNotesByEvent = id => (
+  async (dispatch) => {
+    try {
+      const response = await axios.get('/api/v1/notes/', { params: { eventID: id } });
+      dispatch({
+        type: GET_NOTES_BY_EVENT,
+        payload: response.data,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+);
+
+export const submitVisitors = ({ id, visitors }) => (
+  async () => {
+    try {
+      const response = await axios.patch(`/api/v1/events/${id}/`, {
+        visitors
       });
     } catch (e) {
       console.log(e);
--- src/actions/types.js
+++ src/actions/types.js
@@ -13,3 +13,9 @@ export const SELECT_NEWS = 'select_news';
 
 export const GET_EVENTS = 'get_events';
 export const GET_EVENT_BY_ID = 'get_event_by_id';
+
+export const GET_TRAINEES = 'get_trainees';
+export const GET_TRAINEE_BY_ID = 'get_trainee_by_id';
+export const VISITOR_CHANGE = 'visitor_change';
+
+export const GET_NOTES_BY_EVENT = 'get_notes_by_event';
