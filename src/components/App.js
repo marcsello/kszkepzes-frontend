42d6d827ebc003ffb1b343536b1d715a5dec20f3
Bereczki Sandor <bsandor453@gmail.com>
2019-01-20 02:08:23 +0100

    Fix footer
    Footer should be always fixed to the bottom, though you may need to use margins at the bottom element.

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,16 @@ import Main from './Main';
 import Footer from './Footer';
 
 const App = () => (
-  <div>
-    <Header />
-    <Main />
-    <Footer />
+  <div style={{ minHeight: '100%', position: 'relative' }}>
+    <header id='header' >
+      <Header />
+    </header>
+    <main id='main'>
+      <Main />
+    </main>
+    <footer id='footer' style={{ position: 'absolute', width: '100%', bottom: '0' }}>
+      <Footer />
+    </footer>
   </div>
 );
 
