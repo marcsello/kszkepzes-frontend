1f86464bd406684ad2be4f286324e95000e1f9b5
marcsello <punkosdmarcell@rocketmail.com>
2020-02-08 19:14:02 +0100

    added shadow to front page stuff

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -58,6 +58,7 @@ class Home extends Component {
                   fontSize: '3.5vw',
                   fontWeight: 'normal',
                   marginBottom: 0,
+                  textShadow: "black 0 0 10px",
                   marginTop: '0.5vw',
                 }}
               />
@@ -69,6 +70,7 @@ class Home extends Component {
                   fontSize: '4.5vw',
                   fontWeight: 'bold',
                   marginBottom: '0.5vw',
+                  textShadow: "black 0 0 10px",
                   marginTop: '0.5vw',
                 }}
               />
@@ -80,6 +82,7 @@ class Home extends Component {
                   fontSize: '3vw',
                   fontWeight: 'normal',
                   marginBottom: 0,
+                  textShadow: "black 0 0 10px",
                   marginTop: '0.5vw',
                 }}
               />
@@ -96,6 +99,7 @@ class Home extends Component {
                   fontSize: '3vw',
                   fontWeight: 'normal',
                   marginBottom: 0,
+                  textShadow: "black 0 0 10px",
                   marginTop: '1vw',
                 }}
               />
@@ -124,6 +128,7 @@ class Home extends Component {
                       style={{
                         fontSize: '2vw',
                         marginTop: '1vw',
+                        boxShadow: "black 0 0 10px",
                         marginBottom: '1vw',
                       }}
                     >
